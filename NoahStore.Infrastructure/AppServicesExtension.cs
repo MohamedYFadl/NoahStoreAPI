a189d58ee01970f4aec66f55b0e2556f92d280c5
MomoYasser0 <myasoo28@gmail.com>
2025-03-15 19:10:45 +0200

    Add ServiceExtension

---
+++ NoahStore.Infrastructure/AppServicesExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NoahStore.Core.Interfaces;
+using NoahStore.Infrastructure.Data.DbContexts;
+using NoahStore.Infrastructure.Repositories;
+
+namespace NoahStore.Infrastructure
+{
+    public static class AppServicesExtension
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services
+        }
+    }
+}
