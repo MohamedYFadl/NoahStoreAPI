1457215130f2da7b8a52474191e5c8c058601990
MomoYasser0 <myasoo28@gmail.com>
2025-03-24 17:47:11 +0200

    Replace X-XSS-Protection with Content-Security-Policy, Add Referrer-Policy, Add Permissions-Policy

--- NoahStore.API/Middleware/ExceptionMiddleware.cs
+++ NoahStore.API/Middleware/ExceptionMiddleware.cs
@@ -89,8 +89,36 @@ namespace NoahStore.API.Middleware
         private void ApplySecurity(HttpContext context)
         {
             context.Response.Headers["X-Content-Type-Options"] = "nosniff";
-            context.Response.Headers["X-XSS-Protection"] = "1;mode=block";
+            //context.Response.Headers["X-XSS-Protection"] = "1;mode=block";
             context.Response.Headers["X-Frame-Options"] = "DENY";
+            context.Response.Headers["Content-Security-Policy"] =
+                    "default-src 'self'; " +           
+                    "script-src 'self' 'unsafe-inline'; " + 
+                    "style-src 'self' 'unsafe-inline'; " +  
+                    "img-src 'self' data:; " +         
+                    "font-src 'self'; " +              
+                    "object-src 'none'; " +            
+                    "frame-ancestors 'none'; " +       
+                    "block-all-mixed-content;";
+            context.Response.Headers["Referrer-Policy"] = "strict-origin-when-cross-origin";
+            context.Response.Headers["Permissions-Policy"] =
+                    "accelerometer=(), " +        
+                    "ambient-light-sensor=(), " +
+                    "autoplay=(), " +
+                    "camera=(), " +
+                    "display-capture=(), " +
+                    "encrypted-media=(), " +
+                    "fullscreen=(), " +
+                    "geolocation=(), " +
+                    "gyroscope=(), " +
+                    "magnetometer=(), " +
+                    "microphone=(), " +
+                    "midi=(), " +
+                    "payment=(), " +
+                    "picture-in-picture=(), " +
+                    "speaker=(), " +
+                    "usb=(), " +
+                    "vr=()";
         }
     }
 }
