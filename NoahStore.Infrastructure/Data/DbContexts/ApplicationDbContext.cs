1e43dbe79bd54237e7e12ad9684b5e931c25dfe0
MomoYasser0 <myasoo28@gmail.com>
2025-04-25 15:39:58 +0300

    Create AppUser Class and add Identity Table to database

--- NoahStore.Infrastructure/Data/DbContexts/ApplicationDbContext.cs
+++ NoahStore.Infrastructure/Data/DbContexts/ApplicationDbContext.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using NoahStore.Core.Entities;
+using NoahStore.Core.Entities.Identity;
 using System.Reflection;
 
 
 namespace NoahStore.Infrastructure.Data.DbContexts
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -16,6 +18,7 @@ namespace NoahStore.Infrastructure.Data.DbContexts
         public virtual DbSet<ProductImage> ProductImages { get; set; }
 
         public virtual DbSet<ProductReview> ProductReviews   { get; set; }
+        public virtual DbSet<Address> Addresses { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
